63afbb550925d01bba4d07a63960b24a87a26373
almmessias <alm.messias@gmail.com>
2022-03-10 07:12:40 -0300

    correcao ex

--- ex070.py
+++ ex070.py
@@ -1,16 +1,24 @@
 print ('Lojas Luiza')
 print ('+' * 40)
-stotal = cproduto = barato = 0
-nproduto = 'nenhum'
+stotal = cproduto = barato = cont = 0
+nproduto = ''
 while True:
     nome = str(input('Digite o nome do produto: ')).strip().upper()
     preco = float(input ('Digite o preço do produto: R$'))
-    continua = str(input('Quer continuar? S/N ')).upper().strip()[0]
-    if continua not in 'SN':
+    continua = ' '
+    while continua not in 'SN':
         continua = str(input('Quer continuar? S/N ')).upper().strip()[0]
     if preco > 1000:
         cproduto += 1
     stotal += preco
+    cont += 1
+    if cont == 1:
+        barato = preco
+        nproduto = nome
+    else:
+        if preco < barato:
+            barato = preco
+            nproduto = nome
     print ('=' * 40)
     if continua == 'N':
         print ('=' * 40)
@@ -18,4 +26,4 @@ while True:
         break
 print (f'O total das compras foi R${stotal:.2f}')
 print (f'Teve {cproduto} acima de R$1.000.')
-print (f'O produto mais barato foi {nproduto} custando R${barato}.')
+print (f'O produto mais barato foi {nproduto} custando R${barato:2f}.')
--- ex071.py
+++ ex071.py
@@ -1,3 +1,22 @@
-print ('BANCO ALM')
+print ('{:-^40}'.format(' BANCO ALM '))
 print ('*' * 40)
-saque = int (input ('Qual valor quer sacar: R$'))
+saque = int (input ('Qual valor você quer sacar: R$'))
+cedula = 50
+total = saque
+totced = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totced += 1
+    else:
+        if totced > 0:
+            print (f'Total de {totced} cédulas de R${cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totced = 0
+        if total == 0:
+            break
