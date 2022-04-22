5e67aa58332089c86e9cac7333a59605234509a4
almmessias <alm.messias@gmail.com>
2022-04-22 06:59:04 -0300

    correção exercicio

--- ex098.py
+++ ex098.py
@@ -1,11 +1,30 @@
 from time import sleep
-#def contador(i, f, p):
-i = 0
-f = 10
-p = 1
-while i >= f:
-    print (i)
-    i += p
+def contador(i, f, p):
+    if p == 0:
+        p = 1
+    if p < 0:
+        p *= -1    
+    print ('-=' * 30)
+    print (f'Contagem de {i} até {f} de {p} em {p}')
+    if i < f:
+        while i <= f:
+            print (i, end=' ', flush=True)
+            sleep(0.2)
+            i += p
+    else:
+        while i >= f:
+            print (i, end=' ', flush=True)
+            sleep(0.2)
+            i -= p
+    print ('FIM')
 
 
-#contador(0, 10, 1)
+contador(0, 10, 1)
+contador(10, 0, 2)
+print ('-=' * 30)
+print ('Agora é a sua vez de personalizar a contagem!')
+ini = int(input('Inicio: '))
+fim = int(input('Fim:    '))
+pas = int(input('Passo:  '))
+contador(ini, fim, pas)
+
