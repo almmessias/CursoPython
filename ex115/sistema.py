e23e9a61a322b7849182d2c1315e5d7de1ad8fda
almmessias <alm.messias@gmail.com>
2022-05-31 06:43:33 -0300

    exercicios

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,9 +1,16 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do sistema'])
     if resposta == 1:
-        cabecalho('Ver Pessoas Cadastradas')
+        #cabecalho('Ver Pessoas Cadastradas')
+        lerArquivo(arq)
     elif resposta == 2:
         cabecalho('Cadastrar Nova Pessoa')
     elif resposta == 3:
